828e6934d7b4906237867d38547a9fc6eefd9aaa
Robinson Amianda <robinsonamianda5@gmail.com>
2023-11-27 21:52:32 +0300

    conference_badges

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,9 +1,9 @@
 def badge_maker(name):
-    return None
+    return  f"`Hello,my name is {name}.`"
 
-def batch_badge_creator(names):
-    return None
-
+def batch_badge_creator(names,name):
+    print(  f"`Hello,my name is {names}.`" , f"`Hello,my name is {name}.`")
+batch_badge_creator("Robin","Simon")
 def assign_rooms(names):
     return None
 
